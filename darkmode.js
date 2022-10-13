b229e6e2e7f5284a103b4c31f54499a7090d1f76
Deivid Daniel <79475211+dev-br18@users.noreply.github.com>
2022-10-12 23:32:58 -0300

    atualizaçao darkmod

---
+++ darkmode.js
@@ -0,0 +1,34 @@
+const html = document.querySelector('html');
+
+
+const checkbox = document.querySelector('input[name=theme]');
+
+const getStyle = (element, style) => {
+    window.getComputedStyle(element).getPropertyPriority(style)
+}
+
+const initialColors = {
+    pn: getStyle(html, '--pn'),
+    bn: getStyle(html, '--bn'),
+    txt: getStyle(html, '--txt'),
+    pl: getStyle(html, '--pl')
+}
+
+const darkmode = {
+    pn: '#414141',
+    bn: '#414141',
+    txt: '#FEFEFE',
+    pl: '#000000'
+}
+
+const transformKey = key => '--' + key.replace(/([A-Z])/, '-$1').toLowerCase()
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => {
+        html.style.setProperty(transformKey(key), colors[key])
+    })
+}
+
+checkbox.addEventListener('change', ({ target }) => {
+    target.checked ? changeColors(darkmode) : changeColors(initialColors)
+})
